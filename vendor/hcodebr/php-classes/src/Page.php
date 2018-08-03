c3593096995b0e6e6fe9fd72e737d73ffe4f1257
lindomar1016 <lindomar1016@hotmail.com>
2018-08-03 10:43:53 -0300

    Criado a classe Page

---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+
+class Page{
+
+	private $tpl;
+	private $defaults = [      //Configurações padroes
+		"data" => []];
+	private $options = [];
+
+	public function  __construct($opts = array()){
+
+		$this->options = array_merge($this->defaults, $opts); //mesclando novas opçoes recebidas com as padroes
+
+
+		$config = array(
+					"tpl_dir"       => $_SERVER['DOCUMENT_ROOT']."/views/", 			// local do template
+					"cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",		// pasta cache
+					"debug"         => false // set to false to improve the speed
+				   );
+
+		Tpl::configure( $config );
+
+		$this->tpl = new Tpl;
+
+		$this->setData($this->options["data"]); //passar os dados para o template
+
+		$this->tpl->draw("header"); //colocando na pagina o header.
+	}
+
+	private function setData($data = array()){
+		foreach ($data as $key => $value) { //Percorrendo cada dado recebido ;
+			$this->tpl->assign($key, $value);				 
+		}
+	}
+	public function setTpl($name, $data = array(), $returnHTML = false){ //HTML que vai ser trabalhado
+
+		$this->setData($data);
+
+		return $this->tpl->draw($name, $returnHTML);
+	}
+
+
+	public function __destruct(){
+
+		$this->tpl->draw("footer"); //colocando o footer na pagina
+
+	}
+
+
+
+}
+?>
