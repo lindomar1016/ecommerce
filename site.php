de88225905bb3efbea2efda007bfe9759064ac39
lindomar1016 <lindomar1016@hotmail.com>
2018-08-13 12:24:42 -0300

    Site- produtos

--- site.php
+++ site.php
@@ -1,13 +1,20 @@
 <?php
 
 use Hcode\Page;
+use Hcode\Model\Product;
+use Hcode\Model\Category;
 
 $app->get('/', function() { //MONTANDO ROTA TEMPLATE SITE
     
     Category::updateFile();
 
+
+    $products = Product::listAll();
+  
 	$page = new Page();
-	$page->setTpl("index");
+	$page->setTpl("index",[
+		"products"=>Product::checkList($products)
+	]);
 	
 });
 
